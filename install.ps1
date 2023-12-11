7c433ae02330114ed054ec67e59afc089f66ec95
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2023-12-11 21:20:07 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -99,6 +99,7 @@ Function preinstallation {
 }
 #installing required fonts
 Function installfonts {
+	 If (-not(Test-Path "$env:LOCALAPPDATA\Microsoft\Windows\Fonts\CaskaydiaCoveNerdFontMono-Regular.ttf")) {
 $fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families
 Write-Output "Installing required fonts..."
 # Check if CaskaydiaCove NF is installed #Christitus script
@@ -110,7 +111,7 @@ if ($fontFamilies -notcontains "CaskaydiaCove NF") {
     Expand-Archive -Path ".\CascadiaCode.zip" -DestinationPath ".\CascadiaCode" -Force
     $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
     Get-ChildItem -Path ".\CascadiaCode" -Recurse -Filter "*.ttf" | ForEach-Object {
-        If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {        
+        If (-not(Test-Path "$env:WINDIR\Fonts\$($_.Name)")) {        
             # Install font
             $destination.CopyHere($_.FullName, 0x10)
         }
@@ -118,6 +119,7 @@ if ($fontFamilies -notcontains "CaskaydiaCove NF") {
     # Clean up
     Remove-Item -Path ".\CascadiaCode" -Recurse -Force -ErrorAction SilentlyContinue
     Remove-Item -Path ".\CascadiaCode.zip" -Force -ErrorAction SilentlyContinue
+		}
     } else {
     Write-Output "Font found, skipping download..."
 	}
