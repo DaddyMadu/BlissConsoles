ae11532de75c2d4252102c98ec82bdd3e95bb5e4
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2023-12-11 15:34:41 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -22,10 +22,10 @@ $installationsteps = @(
 
 #check administrator privilage
 Function RequireAdmin {
-	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
-		Exit
-	}
+	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	Exit
+}
 }
 #Create Restore Point
 Function CreateRestorePoint {
