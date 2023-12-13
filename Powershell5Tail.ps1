f042c91b0dde573aacd658aea6d800cebfb701cf
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2023-12-13 11:47:00 +0200

    Create Powershell5Tail.ps1

---
+++ Powershell5Tail.ps1
@@ -0,0 +1,19 @@
+Import-Module PSReadLine;
+function prompt {
+  if (Test-Path variable:global:ompjob) {
+    Receive-Job -Wait -AutoRemoveJob -Job $global:ompjob | Invoke-Expression;
+    Remove-Variable ompjob -Scope Global;
+    Enable-PoshTransientPrompt
+    Enable-PoshLineError
+
+    Set-PSReadLineOption -EditMode Windows
+    Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+    Set-PSReadLineOption -PredictionViewStyle InlineView
+
+    [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+    return prompt;
+  }
+  $global:ompjob = Start-Job {Import-Module -Name Terminal-Icons; oh-my-posh --init --shell powershell --config $env:POSH_THEMES_PATH/kushal.omp.json};
+  Write-Host "BC v1.3 Loading Profile in Background..."
+  return " ";
+}
