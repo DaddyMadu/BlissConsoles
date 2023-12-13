fc8eaae4a35f097e7e390770b99a1151608f52ff
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2023-12-13 16:32:38 +0200

    Update Powershell5Tail.ps1

--- Powershell/Powershell5Tail.ps1
+++ Powershell/Powershell5Tail.ps1
@@ -12,7 +12,7 @@ function prompt {
     [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
     return prompt;
   }
-  $global:ompjob = Start-Job {Import-Module -Name Terminal-Icons; oh-my-posh --init --shell powershell --config $env:POSH_THEMES_PATH/kushal.omp.json};
+  $global:ompjob = Start-Job {oh-my-posh --init --shell powershell --config $env:POSH_THEMES_PATH/kushal.omp.json};
   Write-Host "BC v1.5 Loading Profile in Background..."
   Write-Host -NoNewline "$($executionContext.SessionState.Path.CurrentLocation)".replace($pwd, '~');
   Write-Host -NoNewline " --->";
