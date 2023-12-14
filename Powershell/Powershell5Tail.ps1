e5c981f895176193f436da0ef9e4b67da31fadd2
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2023-12-14 08:33:45 +0300

    Update Powershell5Tail.ps1

--- Powershell/Powershell5Tail.ps1
+++ Powershell/Powershell5Tail.ps1
@@ -1,3 +1,12 @@
+$nficon=[PSCustomObject]@{
+'/\/'=[char]0xf489
+}
+$nfcleanicon = $nficon.'/\/'
+
+$nficon2=[PSCustomObject]@{
+'/\/\'=[char]0xe7a2
+}
+$nfcleanicon2 = $nficon2.'/\/\'
 function prompt {
   if (Test-Path variable:global:ompjob) {
     Receive-Job -Wait -AutoRemoveJob -Job $global:ompjob | Invoke-Expression;
@@ -14,7 +23,7 @@ function prompt {
   }
   $global:ompjob = Start-Job {oh-my-posh --init --shell powershell --config $env:POSH_THEMES_PATH/kushal.omp.json};
   Write-Host "BC v1.6 Loading Profile in Background..."
-  Write-Host -NoNewline "$($executionContext.SessionState.Path.CurrentLocation)".replace($pwd, '~');
-  Write-Host -NoNewline " --->";
+  Write-Host -NoNewline $nfcleanicon "$($executionContext.SessionState.Path.CurrentLocation)".replace($pwd, '~');
+  Write-Host -NoNewline " $nfcleanicon2";
   return " ";
 }
