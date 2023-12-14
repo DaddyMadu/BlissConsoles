d9a5a8115dd0b7401463cae1ef13f5e28f5ef29a
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2023-12-14 08:52:10 +0300

    Update Powershell7Tail.ps1

--- Powershell/Powershell7Tail.ps1
+++ Powershell/Powershell7Tail.ps1
@@ -15,6 +15,22 @@ function prompt {
   $ohmyposhluncher = & ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\kushal.omp.json" --print) -join "`n"))
   $global:ompjob = Start-Job {$ohmyposhluncher};
   write-host -ForegroundColor Blue "BC v1.6 Loading `$profile in the background..."
+  Write-Host '           _________________' -ForegroundColor Red
+Write-Host '         < Welcome' $Env:UserName'!! >' -ForegroundColor Green
+Write-Host '            -----------------' -ForegroundColor Red
+Write-Host '             \' -ForegroundColor Red
+Write-Host '              \' -ForegroundColor Red
+Write-Host '                                            .::!!!!!!!:.' -ForegroundColor Red
+Write-Host '           .!!!!!:.                        .:!!!!!!!!!!!!' -ForegroundColor Red
+Write-Host '           ~~~~!!!!!!.                 .:!!!!!!!!!UWWW$$$' -ForegroundColor Red
+Write-Host '               :$$NWX!!:           .:!!!!!!XUWW$$$$$$$$$P' -ForegroundColor Red
+Write-Host '               $$$$$##WX!:      .<!!!!UW$$$$"  $$$$$$$$#' -ForegroundColor Red
+Write-Host '               $$$$$  $$$UX   :!!UW$$$$$$$$$   4$$$$$*' -ForegroundColor Red
+Write-Host '               ^$$$B  $$$$\     $$$$$$$$$$$$   d$$R"' -ForegroundColor Red
+Write-Host '                 "*$bd$$$$       "#$$$$$$$$$$bd$P' -ForegroundColor Red
+Write-Host '                      """"          """"""""""" ' -ForegroundColor Red
+Write-Host ' '
+Write-Host ' '
   Write-Host -ForegroundColor Green -NoNewline "  $($executionContext.SessionState.Path.CurrentLocation) ".replace($HOME, '~');
   Write-Host -ForegroundColor Red -NoNewline "ᓚᘏᗢ"
   return " ";
