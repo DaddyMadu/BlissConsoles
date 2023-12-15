3fc0a99689b63887e50b858d2ab3390a0f24d2eb
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2023-12-15 03:45:42 +0300

    Update Powershell5Tail.ps1

--- Powershell/Powershell5Tail.ps1
+++ Powershell/Powershell5Tail.ps1
@@ -23,7 +23,23 @@ function prompt {
   }
   $global:ompjob = Start-Job {oh-my-posh --init --shell powershell --config $env:POSH_THEMES_PATH/kushal.omp.json};
   Write-Host "BC v1.6 Loading Profile in Background..."
-  Write-Host -NoNewline $nfcleanicon "$($executionContext.SessionState.Path.CurrentLocation)".replace($pwd, '~');
-  Write-Host -NoNewline " $nfcleanicon2";
+    Write-Host '            _________________' -ForegroundColor "Red";
+Write-Host '         < Welcome' $Env:UserName'!! >' -ForegroundColor "Green";
+Write-Host '            -----------------' -ForegroundColor "Red";
+Write-Host '             \' -ForegroundColor "Red";
+Write-Host '              \' -ForegroundColor "Red";
+Write-Host '                                            .::!!!!!!!:.' -ForegroundColor "Red";
+Write-Host '           .!!!!!:.                        .:!!!!!!!!!!!!' -ForegroundColor "Red";
+Write-Host '           ~~~~!!!!!!.                 .:!!!!!!!!!UWWW$$$' -ForegroundColor "Red";
+Write-Host '               :$$NWX!!:           .:!!!!!!XUWW$$$$$$$$$P' -ForegroundColor "Red";
+Write-Host '               $$$$$##WX!:      .<!!!!UW$$$$"  $$$$$$$$#' -ForegroundColor "Red";
+Write-Host '               $$$$$  $$$UX   :!!UW$$$$$$$$$   4$$$$$*' -ForegroundColor "Red";
+Write-Host '               ^$$$B  $$$$\     $$$$$$$$$$$$   d$$R"' -ForegroundColor "Red";
+Write-Host '                 "*$bd$$$$       "#$$$$$$$$$$bd$P' -ForegroundColor "Red";
+Write-Host '                      """"          """"""""""" ' -ForegroundColor "Red";
+Write-Host ' ';
+Write-Host ' ';
+  Write-Host -NoNewline $nfcleanicon "$($executionContext.SessionState.Path.CurrentLocation)".replace($pwd, '~') -foregroundcolor "Green";
+  Write-Host -NoNewline " $nfcleanicon2" -foregroundcolor "red";
   return " ";
 }
