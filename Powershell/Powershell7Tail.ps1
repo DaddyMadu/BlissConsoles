2d550c2289debb177a121960e14a5d424a03aabb
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2023-12-15 06:46:33 +0300

    Update Powershell7Tail.ps1

--- Powershell/Powershell7Tail.ps1
+++ Powershell/Powershell7Tail.ps1
@@ -14,7 +14,7 @@ function prompt {
   }
   $ohmyposhluncher = & ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\kushal.omp.json" --print) -join "`n")) && Import-Module Terminal-Icons
   $global:ompjob = Start-Job {$ohmyposhluncher};
-  write-host -ForegroundColor Blue "BC v1.6 Loading `$profile in the background..."
+  write-host -ForegroundColor Blue "BC v1.7 Loading `$profile in the background..."
   Write-Host '            _________________' -ForegroundColor Red
 Write-Host '         < Welcome' $Env:UserName'!! >' -ForegroundColor Green
 Write-Host '            -----------------' -ForegroundColor Red
